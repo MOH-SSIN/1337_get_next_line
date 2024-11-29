e081e4a3693a252c240f257677110ed8d539ecdc
MOH-SSIN <mohssinzahi99@gmail.com>
2024-11-29 12:34:47 +0100

    get_next_line_1337

--- get_next_line.h
+++ get_next_line.h
@@ -13,11 +13,11 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdio.h>
+// # include <stdio.h>
 # include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
-#include <limits.h>
+// # include <unistd.h>
+// # include <fcntl.h>
+# include <limits.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10000000
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -13,10 +13,11 @@
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
-# include <stdio.h>
+// # include <stdio.h>
 # include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
+// # include <unistd.h>
+// # include <fcntl.h>
+# include <limits.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10000000
